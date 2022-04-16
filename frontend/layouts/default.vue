6f175476894445064037689250e9bdf441a95c53
TArch64 <tarasturchenko1551@gmail.com>
2022-04-16 20:52:52 +0300

    add button navigation

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,15 +1,38 @@
 <template>
   <v-app>
-    <v-app-bar fixed app>
-      <v-toolbar-title>
-        Поїздки
-      </v-toolbar-title>
-    </v-app-bar>
-
     <v-main>
       <v-container>
         <Nuxt />
       </v-container>
     </v-main>
+
+    <v-bottom-navigation fixed>
+      <v-btn
+        v-for="(item, index) of $options.navItems"
+        :key="item.url"
+        :to="item.url"
+        :class="{ 'ml-2': index > 0 }"
+      >
+        <span>{{ item.title }}</span>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
   </v-app>
 </template>
+
+<script>
+export default {
+  navItems: [
+    {
+      title: 'Нові',
+      icon: 'mdi-car',
+      url: '/'
+    },
+    {
+      title: 'Активні',
+      icon: 'mdi-play',
+      url: '/active'
+    }
+  ]
+}
+</script>
