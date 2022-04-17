96570fc94a029a7736449cc70ecb079a45731a0c
TArch64 <tarasturchenko1551@gmail.com>
2022-04-17 10:59:48 +0300

    add production configs

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker compose down
-git pull
-docker image rm $(docker image ls -aq)
-docker compose build
-docker compose run frontend npm i
-docker compose run backend npm i
-docker compose up -d
