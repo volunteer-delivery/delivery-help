1a973da37e1aad3a102cdff8ffb67902f711a523
TArch64 <tarasturchenko1551@gmail.com>
2022-04-23 18:42:17 +0300

    fix envs

--- deployment/production/deploy.sh
+++ deployment/production/deploy.sh
@@ -4,4 +4,4 @@ export $(cat .env | xargs) && \
 echo $CR_PASSWORD | docker login ghcr.io -u $CR_USERNAME --password-stdin && \
 docker compose -f ./docker-compose-build.yaml build && \
 docker compose -f ./docker-compose-build.yaml push && \
-./update.sh
+./connect.sh "$(cat ./deploy-update-remote.sh)"
--- deployment/production/update.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-export $(cat .env | xargs) && \
-ssh -i "$REMOTE_KEY" "$REMOTE_USER@$REMOTE_HOST" -t "$(cat ./update-remote.sh)"
