f65b7677d8218245d1bba6b3c7bee97dde303198
TArch64 <tarasturchenko1551@gmail.com>
2022-04-26 19:27:23 +0300

    Redirect to login after expire token

--- frontend/plugins/axios.js
+++ frontend/plugins/axios.js
@@ -1,6 +1,11 @@
 export default function (context) {
     context.$axios.onError(error => {
         if (!error.response) return error;
+
+        if (error.response.status === 403) {
+            window.location.reload();
+        }
+
         return Promise.reject(error.response.data);
     });
 }
