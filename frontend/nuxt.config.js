7a2f628be6eb4e0e42b98dc28e7c389768eb26db
TArch64 <tarasturchenko1551@gmail.com>
2022-05-17 18:25:03 +0300

    fix html сaching

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -24,8 +24,7 @@ export default {
 
         meta: [
             { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { 'http-equiv': 'Content-Security-Policy', content: "default-src 'unsafe-inline' 'self'; style-src-elem 'unsafe-inline' 'self' https://fonts.googleapis.com; font-src https://fonts.gstatic.com;" }
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' }
         ],
 
         link: [
