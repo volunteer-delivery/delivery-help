394483b5fe9aa1c2168753d8589f84c17bb127b7
TArch64 <tarasturchenko1551@gmail.com>
2022-05-23 21:50:16 +0300

    add telegram noty

--- deployment/staging/deploy-update-remote.sh
+++ deployment/staging/deploy-update-remote.sh
@@ -4,13 +4,20 @@ project_dir="$HOME/projects/delivery-help";
 docker_compose_file="$project_dir/deployment/staging/docker-compose-up.yaml";
 
 function exec_docker() {
-    docker compose --file "$docker_compose_file"  --project-directory "$project_dir" $1
+  docker compose --file "$docker_compose_file"  --project-directory "$project_dir" $1
 }
 
-rm -rf "$project_dir" && \
-git clone --branch "$project_branch" "$project_repository" "$project_dir" && \
+function pull_sources() {
+  rm -rf "$project_dir" && git clone --branch "$project_branch" "$project_repository" "$project_dir"
+}
+
+function docker_login() {
+  echo "$CR_PASSWORD" | docker login ghcr.io -u "$CR_USERNAME" --password-stdin
+}
+
 export $(cat "$HOME/projects/.env" | xargs) && \
-echo "$CR_PASSWORD" | docker login ghcr.io -u "$CR_USERNAME" --password-stdin && \
+pull_sources && \
+docker_login && \
 exec_docker "pull" && \
 exec_docker "down --remove-orphans" && \
 exec_docker "up --detach"
