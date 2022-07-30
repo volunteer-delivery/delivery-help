51491365e07ba839cddcbfc8ce36c8620dd69193
TArch64 <tarasturchenko1551@gmail.com>
2022-07-30 17:55:37 +0300

    refactor ci structure

--- ci/common/hooks/before-install.sh
+++ ci/common/hooks/before-install.sh
@@ -2,4 +2,4 @@
 
 export $(cat "$HOME/delivery-help/.env" | xargs) && \
 echo $CR_PASSWORD | docker login $CR_HOST -u $CR_USERNAME --password-stdin && \
-docker compose -f ../staging/docker-compose.yaml pull;
+docker compose -f ../../staging/docker-compose.yaml pull;
---
+++ ci/common/hooks/stop.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+
+docker compose -f ../../staging/docker-compose.yaml down --remove-orphans;
