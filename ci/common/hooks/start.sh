807404ebe124ba7b30a29a9e949807b9989a55f6
TArch64 <tarasturchenko1551@gmail.com>
2022-07-30 21:43:40 +0300

    Fix loading envs

--- ci/common/hooks/start.sh
+++ ci/common/hooks/start.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env sh
 
-config="$HOME/delivery-help/app/ci/staging/docker-compose.yaml";
+root="$HOME/delivery-help"
+config="$root/app/ci/staging/docker-compose.yaml";
 
+export $(cat "$root/.env" | xargs) && \
+echo $CR_PASSWORD | docker login $CR_HOST -u $CR_USERNAME --password-stdin && \
 docker compose -f "$config" up --detach && \
 docker compose -f "$config" down --remove-orphans;
