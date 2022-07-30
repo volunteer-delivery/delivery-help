2bc535202bfa1b78bd5c7f935f262c74722fbc5f
TArch64 <tarasturchenko1551@gmail.com>
2022-07-30 21:36:15 +0300

    use absolute path

--- ci/common/hooks/after-install.sh
+++ ci/common/hooks/after-install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
-export $(cat "$HOME/delivery-help/.env" | xargs) && \
+root="$HOME/delivery-help"
+
+export $(cat "$root/.env" | xargs) && \
 echo $CR_PASSWORD | docker login $CR_HOST -u $CR_USERNAME --password-stdin && \
-docker compose -f ci/staging/docker-compose.yaml pull;
+docker compose -f "$root/app/ci/staging/docker-compose.yaml" pull;
--- ci/common/hooks/start.sh
+++ ci/common/hooks/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-config="ci/staging/docker-compose.yaml";
+config="$HOME/delivery-help/app/ci/staging/docker-compose.yaml";
 
 docker compose -f "$config" up --detach && \
 docker compose -f "$config" down --remove-orphans;
