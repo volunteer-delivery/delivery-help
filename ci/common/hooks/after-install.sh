0841123246a175c89d91393dd4f1c5b75fedd1b1
TArch64 <tarasturchenko1551@gmail.com>
2022-07-30 20:55:43 +0300

    use relative url

--- ci/common/hooks/after-install.sh
+++ ci/common/hooks/after-install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
 
-root="$HOME/delivery-help"
-
-export $(cat "root/.env" | xargs) && \
+export $(cat "$HOME/delivery-help/.env" | xargs) && \
 echo $CR_PASSWORD | docker login $CR_HOST -u $CR_USERNAME --password-stdin && \
-docker compose -f "$root/app/ci/staging/docker-compose.yaml" pull;
+docker compose -f ci/staging/docker-compose.yaml pull;
