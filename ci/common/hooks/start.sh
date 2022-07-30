43de0bf72035ccac5316f2daef7dfd0e3e25cbee
TArch64 <tarasturchenko1551@gmail.com>
2022-07-30 21:50:14 +0300

    Fix start command

--- ci/common/hooks/start.sh
+++ ci/common/hooks/start.sh
@@ -5,5 +5,5 @@ config="$root/app/ci/staging/docker-compose.yaml";
 
 export $(cat "$root/.env" | xargs) && \
 echo $CR_PASSWORD | docker login $CR_HOST -u $CR_USERNAME --password-stdin && \
-docker compose -f "$config" up --detach && \
-docker compose -f "$config" down --remove-orphans;
+docker compose -f "$config" down --remove-orphans && \
+docker compose -f "$config" up --detach;
