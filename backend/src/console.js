da9d5464cd4452532da9dee42f484f932877ea9e
TArch64 <tarasturchenko1551@gmail.com>
2022-08-31 21:02:55 +0300

    add console script

---
+++ backend/src/console.js
@@ -0,0 +1,24 @@
+const repl = require('repl');
+const mongoose = require('mongoose');
+const { readFileSync, writeFileSync, existsSync } = require('fs');
+
+(async () => {
+    await mongoose.connect(process.env.MONGO_URL);
+
+    global.deliveryHelp = {
+        mongoose,
+        ...require('./models'),
+        ...require('./services')
+    };
+
+    const replServer = repl.start();
+    const historyLocation = '/tmp/delivery-help-console-history';
+
+    const historyContent = !existsSync(historyLocation) ? '' : readFileSync(historyLocation, { encoding: 'utf8' });
+    const history = historyContent.split('\n').filter((command) => command !== '.exit').join('\n');
+
+    writeFileSync(historyLocation, history);
+
+    replServer.setupHistory(historyLocation, () => {});
+})();
+
