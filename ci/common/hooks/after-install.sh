2069fae888611b5fdad211817e94aea25d12bde5
TArch64 <tarasturchenko1551@gmail.com>
2022-08-31 20:14:08 +0300

    add deploy production configs

--- ci/common/hooks/after-install.sh
+++ ci/common/hooks/after-install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 root="$HOME/delivery-help"
+env="$DEPLOYMENT_GROUP_NAME"
 
 export $(cat "$root/.env" | xargs) && \
 echo $CR_PASSWORD | docker login $CR_HOST -u $CR_USERNAME --password-stdin && \
-docker compose -f "$root/app/ci/staging/docker-compose.yaml" pull;
+docker compose -f "$root/app/ci/$env/docker-compose.yaml" pull;
--- ci/common/hooks/start.sh
+++ ci/common/hooks/start.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 root="$HOME/delivery-help"
-config="$root/app/ci/staging/docker-compose.yaml";
+env="$DEPLOYMENT_GROUP_NAME"
+config="$root/app/ci/$env/docker-compose.yaml";
 
 export $(cat "$root/.env" | xargs) && \
 echo $CR_PASSWORD | docker login $CR_HOST -u $CR_USERNAME --password-stdin && \
