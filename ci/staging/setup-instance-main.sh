ce613f5bc6b4614344650b5339ab30e2a27853cc
TArch64 <tarasturchenko1551@gmail.com>
2022-11-06 22:57:30 +0200

    add progress logs

--- ci/staging/setup-instance-main.sh
+++ ci/staging/setup-instance-main.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
 install_codedeploy() {
+    echo "install codedeploy" && \
     sudo yum install -y ruby wget && \
     wget https://aws-codedeploy-eu-central-1.s3.eu-central-1.amazonaws.com/latest/install -o $HOME/install && \
     chmod +x $HOME/install && \
-    sudo /tmp/install_codedeploy auto && \
+    sudo $HOME/install auto && \
+    echo "start codedeploy" && \
     sudo service codedeploy-agent start && \
     sudo service codedeploy-agent status
 }
@@ -12,17 +14,21 @@ install_codedeploy() {
 install_docker() {
     DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
 
+    echo "install docker" && \
     sudo yum install -y docker && \
+    echo "start docker" && \
+    sudo usermod -a -G docker ec2-user && \
     sudo systemctl start docker.service && \
     sudo systemctl enable docker.service && \
-    sudo usermod -a -G docker ec2-user && \
     docker -v && \
+    echo "install docker compose" && \
     mkdir -p $DOCKER_CONFIG/cli-plugins && \
     curl -SL https://github.com/docker/compose/releases/download/v2.7.0/docker-compose-linux-aarch64 -o $DOCKER_CONFIG/cli-plugins/docker-compose && \
     chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose && \
     docker compose version
 }
 
+echo "yum update" && \
 sudo yum update -y && \
 install_codedeploy && \
 install_docker
