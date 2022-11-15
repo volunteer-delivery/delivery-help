4bf5d11256a24a63fc045bb9690c1cecdd27901a
TArch64 <tarasturchenko1551@gmail.com>
2022-11-15 19:52:58 +0200

    use secrets loader in deploy scripts

--- ci/common/hooks/after-install.sh
+++ ci/common/hooks/after-install.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env sh
 
+secrets_loader_download=https://raw.githubusercontent.com/volunteer-delivery/secrets-loader/main/build/linux-arm64
+
 root="$HOME/delivery-help"
 env="$DEPLOYMENT_GROUP_NAME"
+env_file="$root/.env"
+secrets_loader_bin="$HOME/secrets-loader"
 
 update_secrets() {
-    rm "$root/.env";
-    ssm_json=$(aws ssm get-parameters-by-path --path "/$env" --no-paginate --with-decryption);
-    ssm_vars=$(echo "$ssm_json" | jq -r '.Parameters[] | "export " + (.Name | split("/")[-1] | ascii_upcase | gsub("-"; "_")) + "=" + .Value')
-    echo "$ssm_vars" >> "$root/.env"
+    rm $env_file $secrets_loader_bin && \
+    curl $secrets_loader_download -o $secrets_loader_bin && \
+    chmod +x $secrets_loader_bin && \
+    $secrets_loader_bin get -r eu-central-1 -p /$env -l isntance-main >> "$root/.env"
 }
 
-export $(cat "$root/.env" | xargs) && \
+update_secrets && \
+export $(cat $env_file | xargs) && \
 echo $CR_PASSWORD | docker login $CR_HOST -u $CR_USERNAME --password-stdin && \
 docker compose -f "$root/app/ci/$env/docker-compose.yaml" pull;
