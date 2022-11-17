ece89dd253bb983644f0267548b95e784ed708a3
TArch64 <tarasturchenko1551@gmail.com>
2022-11-17 20:23:26 +0200

    get secrets loader from submodule

--- ci/common/hooks/after-install.sh
+++ ci/common/hooks/after-install.sh
@@ -1,20 +1,12 @@
 #!/usr/bin/env sh
 
-secrets_loader_download=https://raw.githubusercontent.com/volunteer-delivery/secrets-loader/main/build/linux-arm64
-
 root="$HOME/delivery-help"
+secrets_loader_bin="$root/secrets-loader/build/linux-arm64";
 env="$DEPLOYMENT_GROUP_NAME"
 env_file="$root/.env"
-secrets_loader_bin="$HOME/secrets-loader"
 
-update_secrets() {
-    rm $env_file $secrets_loader_bin && \
-    curl $secrets_loader_download -o $secrets_loader_bin && \
-    chmod +x $secrets_loader_bin && \
-    $secrets_loader_bin get -r eu-central-1 -p /$env -l isntance-main >> "$root/.env"
-}
-
-update_secrets && \
+rm $env_file && \
+$secrets_loader_bin get -r eu-central-1 -p /$env -l isntance-main >> "$root/.env" && \
 export $(cat $env_file | xargs) && \
 echo $CR_PASSWORD | docker login $CR_HOST -u $CR_USERNAME --password-stdin && \
 docker compose -f "$root/app/ci/$env/docker-compose.yaml" pull;
