132bef6294be48ae865da7da7f8c10d10373a582
TArch64 <tarasturchenko1551@gmail.com>
2022-11-18 19:56:19 +0200

    revert changes

--- ci/common/hooks/after-install.sh
+++ ci/common/hooks/after-install.sh
@@ -1,12 +1,8 @@
 #!/usr/bin/env sh
 
 root="$HOME/delivery-help"
-secrets_loader_bin="$root/secrets-loader/build/linux-arm64";
 env="$DEPLOYMENT_GROUP_NAME"
-env_file="$root/.env"
 
-rm $env_file && \
-$secrets_loader_bin get -r eu-central-1 -p /$env -l isntance-main >> "$root/.env" && \
-export $(cat $env_file | xargs) && \
+export $(cat "$root/.env" | xargs) && \
 echo $CR_PASSWORD | docker login $CR_HOST -u $CR_USERNAME --password-stdin && \
 docker compose -f "$root/app/ci/$env/docker-compose.yaml" pull;
