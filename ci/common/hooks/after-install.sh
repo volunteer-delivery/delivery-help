5d9cd4916c47562c49008e97ba01f4f7ec034682
TArch64 <tarasturchenko1551@gmail.com>
2022-11-24 23:09:28 +0200

    update deploy script

--- ci/common/hooks/after-install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-root="$HOME/delivery-help"
-env="$DEPLOYMENT_GROUP_NAME"
-
-export $(cat "$root/.env" | xargs) && \
-echo $CR_PASSWORD | docker login $CR_HOST -u $CR_USERNAME --password-stdin && \
-docker compose -f "$root/app/ci/$env/docker-compose.yaml" pull;
