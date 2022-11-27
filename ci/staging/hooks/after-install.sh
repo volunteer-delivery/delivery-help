b09c49432604c59ca816652a48c51f7c3e814108
TArch64 <tarasturchenko1551@gmail.com>
2022-11-27 18:28:10 +0200

    fix after-install script

--- ci/staging/hooks/after-install.sh
+++ ci/staging/hooks/after-install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
+sudo chown -R $USER:$USER . && \
 rm -f .env && \
-secrets-loader get -r $AWS_REGION -p /staging -l instance-main >> .env && \
+./secrets-loader get -r eu-central-1 -p /staging -l instance-main >> .env && \
 export $(cat .env | xargs) && \
 echo $CR_PASSWORD | docker login $CR_HOST -u $CR_USERNAME --password-stdin && \
 docker compose pull;
