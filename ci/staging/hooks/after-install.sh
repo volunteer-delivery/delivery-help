1f2e4145f91e5f9ccdcc6ca0ecb5794754afc7e7
TArch64 <tarasturchenko1551@gmail.com>
2022-11-27 18:48:41 +0200

    use absolute paths

--- ci/staging/hooks/after-install.sh
+++ ci/staging/hooks/after-install.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 
-cd app && \
-sudo chown -R $USER:$USER . && \
-rm -f .env && \
-./secrets-loader get -r eu-central-1 -p /staging -l instance-main >> .env && \
-export $(cat .env | xargs) && \
+root="$HOME/app"
+env_file="$root/.env"
+
+sudo chown -R $USER:$USER $root && \
+rm -f $env_file && \
+./secrets-loader get -r eu-central-1 -p /staging -l instance-main >> $env_file && \
+export $(cat $env_file | xargs) && \
 echo $CR_PASSWORD | docker login $CR_HOST -u $CR_USERNAME --password-stdin && \
 docker compose pull;
--- ci/staging/hooks/start.sh
+++ ci/staging/hooks/start.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
-cd app && \
-export $(cat .env | xargs) && \
+root="$HOME/app"
+env_file="$root/.env"
+
+export $(cat $env_file | xargs) && \
 echo $CR_PASSWORD | docker login $CR_HOST -u $CR_USERNAME --password-stdin && \
 docker compose down --remove-orphans && \
 docker compose up --detach;
