984139175b553205e3a4031999e5659fb885a45d
TArch64 <tarasturchenko1551@gmail.com>
2022-11-27 17:53:53 +0200

    fix remove .env in deploy

--- ci/staging/hooks/after-install.sh
+++ ci/staging/hooks/after-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-rm .env && \
-secrets-loader get -r $AWS_REGION -p /staging -l instance-main >> '.env' && \
-export $(cat '.env' | xargs) && \
+rm -f .env && \
+secrets-loader get -r $AWS_REGION -p /staging -l instance-main >> .env && \
+export $(cat .env | xargs) && \
 echo $CR_PASSWORD | docker login $CR_HOST -u $CR_USERNAME --password-stdin && \
 docker compose pull;
--- ci/staging/hooks/start.sh
+++ ci/staging/hooks/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-export $(cat '.env' | xargs) && \
+export $(cat .env | xargs) && \
 echo $CR_PASSWORD | docker login $CR_HOST -u $CR_USERNAME --password-stdin && \
 docker compose down --remove-orphans && \
 docker compose up --detach;
