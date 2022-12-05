72b12faffa1ef5cf5269c902c24c4015c9d7933a
TArch64 <tarasturchenko1551@gmail.com>
2022-12-05 23:04:17 +0200

    add db push to start script

--- ci/staging/hooks/start.sh
+++ ci/staging/hooks/start.sh
@@ -7,4 +7,5 @@ docker_compose_file="$root/docker-compose.yaml"
 export $(cat $env_file | xargs) && \
 echo $CR_PASSWORD | docker login $CR_HOST -u $CR_USERNAME --password-stdin && \
 docker compose -f $docker_compose_file down --remove-orphans && \
+docker compose -f $docker_compose_file run --rm backend npm run prisma db push && \
 docker compose -f $docker_compose_file up --detach;
