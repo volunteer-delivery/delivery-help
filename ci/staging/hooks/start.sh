50a0c70413256dc50886aece228a2f6c6e2ad21b
TArch64 <tarasturchenko1551@gmail.com>
2023-01-25 00:19:54 +0200

    fix running db migration

--- ci/staging/hooks/start.sh
+++ ci/staging/hooks/start.sh
@@ -6,5 +6,5 @@ docker_compose_file="$root/docker-compose.yaml"
 
 export $(cat "$env_file" | xargs) && \
 echo "$CR_PASSWORD" | docker login "$CR_HOST" -u "$CR_USERNAME" --password-stdin && \
-docker compose -f "$docker_compose_file" run --rm backend npm run prisma db push -- --skip-generate && \
+docker compose -f "$docker_compose_file" run --rm db-util npm run prisma db push -- --skip-generate && \
 docker compose -f "$docker_compose_file" up --detach;
