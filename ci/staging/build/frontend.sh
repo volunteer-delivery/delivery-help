fbdbca984f07c07d723a55e8c72ae9650f5bee1b
TArch64 <tarasturchenko1551@gmail.com>
2023-01-24 23:49:50 +0200

    fix path to frontend dockerfile

--- ci/staging/build/frontend.sh
+++ ci/staging/build/frontend.sh
@@ -17,7 +17,7 @@ docker build \
   --target builder \
   --tag "$FRONTEND_IMAGE-builder:latest" \
   --tag "$FRONTEND_IMAGE-builder:$APP_VERSION" \
-  --file "ci/$ENV/frontend/Dockerfile" \
+  --file "ci/$ENV/build/frontend.Dockerfile" \
   --cache-from "$FRONTEND_IMAGE-builder:latest" \
   --build-arg BUILDKIT_INLINE_CACHE \
   --build-arg CR_BASE \
@@ -32,7 +32,7 @@ docker push -a "$FRONTEND_IMAGE-builder"
 docker build \
   --tag "$FRONTEND_IMAGE:latest" \
   --tag "$FRONTEND_IMAGE:$APP_VERSION" \
-  --file "ci/$ENV/frontend/Dockerfile" \
+  --file "ci/$ENV/build/frontend.Dockerfile" \
   --cache-from "$FRONTEND_IMAGE-builder:$APP_VERSION" \
   --cache-from "$FRONTEND_IMAGE:latest" \
   --build-arg BUILDKIT_INLINE_CACHE \
