414dd100f8ec5d7aad8c75a84fc15a8a97bb8804
TArch64 <tarasturchenko1551@gmail.com>
2023-01-26 20:09:47 +0200

    update production deployment

---
+++ ci/production/build/db-util.Dockerfile
@@ -0,0 +1,29 @@
+ARG REPO_IMAGE
+ARG APP_VERSION
+FROM $REPO_IMAGE/backend-source:$APP_VERSION as builder
+
+ARG CR_LABEL
+LABEL org.opencontainers.image.source $CR_LABEL
+
+WORKDIR /app
+
+RUN npm run build:seed
+RUN npm run build:console
+
+
+ARG REPO_IMAGE
+ARG APP_VERSION
+FROM $REPO_IMAGE/backend-node:$APP_VERSION
+
+ARG CR_LABEL
+LABEL org.opencontainers.image.source $CR_LABEL
+
+WORKDIR /app
+
+COPY --from=builder /app/dist ./dist
+COPY --from=builder /app/libs/prisma/src/client ./dist/db-util/client
+COPY --from=builder /app/node_modules ./node_modules
+COPY ./backend/prisma ./prisma
+COPY ./backend/scripts ./scripts
+COPY ./backend/package.json ./backend/package-lock.json ./
+
---
+++ ci/production/build/frontend.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+FRONTEND_IMAGE="$REPO_IMAGE/frontend"
+
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>";
+echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<";
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>";
+echo "Building front-end";
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>";
+echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<";
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>";
+
+docker pull "$FRONTEND_IMAGE-builder:latest" || true;
+docker pull "$FRONTEND_IMAGE:latest" || true;
+
+docker build \
+  --target builder \
+  --tag "$FRONTEND_IMAGE-builder:latest" \
+  --tag "$FRONTEND_IMAGE-builder:$APP_VERSION" \
+  --file "ci/$ENV/build/frontend.Dockerfile" \
+  --cache-from "$FRONTEND_IMAGE-builder:latest" \
+  --build-arg BUILDKIT_INLINE_CACHE \
+  --build-arg CR_BASE \
+  --build-arg CR_LABEL \
+  --build-arg FRONTEND_API_URL \
+  --build-arg FRONTEND_SOCKET_URL \
+  --build-arg FRONTEND_ENV \
+  --build-arg FRONTEND_BUGSNAG_KEY \
+  . && \
+docker push -a "$FRONTEND_IMAGE-builder"
+
+docker build \
+  --tag "$FRONTEND_IMAGE:latest" \
+  --tag "$FRONTEND_IMAGE:$APP_VERSION" \
+  --file "ci/$ENV/build/frontend.Dockerfile" \
+  --cache-from "$FRONTEND_IMAGE-builder:$APP_VERSION" \
+  --cache-from "$FRONTEND_IMAGE:latest" \
+  --build-arg BUILDKIT_INLINE_CACHE \
+  --build-arg CR_BASE \
+  --build-arg CR_LABEL \
+  --build-arg FRONTEND_API_URL \
+  --build-arg FRONTEND_SOCKET_URL \
+  --build-arg FRONTEND_ENV \
+  --build-arg FRONTEND_BUGSNAG_KEY \
+  . && \
+docker push -a "$FRONTEND_IMAGE";
