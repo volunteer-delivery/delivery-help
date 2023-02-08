77b6d5b6990b49a2b615f16f270251bd4b8d51f5
TArch64 <tarasturchenko1551@gmail.com>
2023-02-08 19:16:00 +0200

    refactor ripple directive

--- frontend/directives/ripple.ts
+++ frontend/directives/ripple.ts
@@ -1,64 +1,68 @@
 import { ObjectDirective } from 'vue';
 
-interface RippleDirective extends ObjectDirective<HTMLButtonElement> {
-    _clickHandler?(event: MouseEvent): void;
-    _activeAnimationTimeout?: any;
+interface IRipplePosition {
+    x: number;
+    y: number;
 }
 
-export const vRipple: RippleDirective = {
-    mounted(el, binding) {
-        const self = (binding.dir as RippleDirective);
+interface IRippleDirectiveState {
+    activeAnimationTimeout: any;
+}
 
-        self._clickHandler = (event: MouseEvent) => {
-            const el = event.currentTarget as HTMLButtonElement;
+const stateMap = new WeakMap<HTMLElement, IRippleDirectiveState>();
 
-            function complete() {
-                el.classList.remove('ripple');
-                el.style.removeProperty('--ripple-size');
-                el.style.removeProperty('--ripple-x');
-                el.style.removeProperty('--ripple-y');
-                el.style.removeProperty('--ripple-duration')
-            }
+function completeRipple(el: HTMLElement): void {
+    el.classList.remove('ripple');
+    el.style.removeProperty('--ripple-size');
+    el.style.removeProperty('--ripple-x');
+    el.style.removeProperty('--ripple-y');
+    el.style.removeProperty('--ripple-duration')
+}
 
-            if (self._activeAnimationTimeout) {
-                clearTimeout(self._activeAnimationTimeout);
-                complete();
-            }
+function renderRipple(el: HTMLElement, position: IRipplePosition): void {
+    const clientRect = el.getBoundingClientRect();
+    const computedStyles = getComputedStyle(el);
 
-            requestAnimationFrame(() => {
-                const clientRect = el.getBoundingClientRect();
-                const computedStyles = getComputedStyle(el);
+    if (computedStyles.position === 'static') {
+        el.classList.add('relative');
+    }
 
-                if (computedStyles.position === 'static') {
-                    el.classList.add('relative');
-                }
+    const size = Math.max(clientRect.height, clientRect.width) * 1.5;
+    el.style.setProperty('--ripple-size', size + 'px');
+    el.style.setProperty('--ripple-x', (position.x - size / 2) + 'px');
+    el.style.setProperty('--ripple-y', (position.y - size / 2) + 'px');
+    el.style.setProperty('--ripple-duration', '400ms')
+    el.classList.add('ripple');
+}
 
-                const size = Math.max(clientRect.height, clientRect.width) * 1.5;
-                el.style.setProperty('--ripple-size', size + 'px');
-                el.style.setProperty('--ripple-x', (event.offsetX - size / 2) + 'px');
-                el.style.setProperty('--ripple-y', (event.offsetY - size / 2) + 'px');
-                el.style.setProperty('--ripple-duration', '400ms')
-                el.classList.add('ripple');
+function clickHandler(event: MouseEvent) {
+    const el = event.currentTarget as HTMLButtonElement;
+    const state = stateMap.get(el);
 
-                self._activeAnimationTimeout = setTimeout(complete, 400);
-            });
-        }
+    if (state?.activeAnimationTimeout) {
+        clearTimeout(state.activeAnimationTimeout);
+        completeRipple(el);
+    }
 
-        if (!el.disabled) {
-            el.addEventListener('click', self._clickHandler);
-        }
+    requestAnimationFrame(() => {
+        renderRipple(el, { x: event.offsetX, y: event.offsetY });
+        const timeoutId = setTimeout(() => completeRipple(el), 400);
+        stateMap.set(el, { activeAnimationTimeout: timeoutId });
+    });
+}
+
+export const vRipple: ObjectDirective<HTMLButtonElement> = {
+    mounted(el) {
+        if (!el.disabled) el.addEventListener('click', clickHandler);
     },
 
-    updated(el, binding) {
-        const self = binding as RippleDirective;
-
+    updated(el) {
         el.disabled
-            ? el.removeEventListener('click', self._clickHandler!)
-            : el.addEventListener('click', self._clickHandler!);
+            ? el.removeEventListener('click', clickHandler!)
+            : el.addEventListener('click', clickHandler!);
     },
 
-    beforeUnmount(el, binding) {
-        const self = (binding.dir as RippleDirective);
-        el.removeEventListener('click', self._clickHandler!);
+    beforeUnmount(el) {
+        el.removeEventListener('click', clickHandler!);
     }
 };
