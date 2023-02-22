f8fe81d38de7054027fc786937c1131ff3a72b27
TArch64 <tarasturchenko1551@gmail.com>
2023-02-22 22:33:34 +0200

    add phone link to card

--- frontend/components/ride-list/ride-list-item.vue
+++ frontend/components/ride-list/ride-list-item.vue
@@ -1,30 +1,40 @@
 <template>
-    <section class="p-4 border-b border-b-slate-300 flex flex-col items-start">
-        <RidePath class="mb-3" :path="ride.path" />
+    <section class="p-4 pr-2 border-b border-b-slate-300 flex">
+        <div class="flex flex-col items-start grow">
+            <RidePath class="mb-3" :path="ride.path" />
 
-        <AppButton
-            class="p-1 -ml-1 mb-2 flex font-medium text-gray-500"
-            :ripple="RippleColor.BLUE_800"
-            @click="openDriverDetails"
-        >
-            <Icon size="24">
-                <AccountBoxRound />
-            </Icon>
+            <AppButton
+                class="p-1 -ml-1 mb-2 flex font-medium text-gray-500"
+                :ripple="RippleColor.BLUE_800"
+                @click="openDriverDetails"
+            >
+                <Icon size="24">
+                    <AccountBoxRound />
+                </Icon>
 
-            <span class="ml-1">
-                {{ ride.driver.name }}
-            </span>
-        </AppButton>
+                <span class="ml-1">
+                    {{ ride.driver.name }}
+                </span>
+            </AppButton>
 
-        <p class="flex m-0 items-center font-medium text-gray-500">
-            <Icon size="24">
-                <DirectionsCarRound />
-            </Icon>
+            <p class="flex m-0 items-center font-medium text-gray-500">
+                <Icon size="24">
+                    <DirectionsCarRound />
+                </Icon>
 
-            <span class="ml-1">
-                {{ vehicleDetails }}
-            </span>
-        </p>
+                <span class="ml-1">
+                    {{ vehicleDetails }}
+                </span>
+            </p>
+        </div>
+
+        <div class="self-end ml-2">
+            <AppButton look="icon" size="md" :to="phoneLink">
+                <Icon class="text-blue-800" size="24">
+                    <LocalPhoneRound />
+                </Icon>
+            </AppButton>
+        </div>
     </section>
 </template>
 
@@ -33,6 +43,7 @@ import type { PropType } from 'vue';
 import { Icon } from '@vicons/utils';
 import AccountBoxRound from '@vicons/material/AccountBoxRound';
 import DirectionsCarRound from '@vicons/material/DirectionsCarRound';
+import LocalPhoneRound from '@vicons/material/LocalPhoneRound';
 import { Ride } from '~/stores/rides-store';
 import { DriverDetailsModal } from '#components';
 
@@ -46,6 +57,7 @@ const props = defineProps({
 const modalStore = useModalStore();
 
 const vehicleDetails = computed(() => formatVehicleDetails(props.ride.vehicle));
+const phoneLink = computed(() => `tel:${props.ride.driver.phone}`);
 
 const openDriverDetails = (): void => void modalStore.open(DriverDetailsModal, {
     props: { driver: props.ride.driver },
