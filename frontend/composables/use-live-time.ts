89f49eb2e630489d6616ed1415d637f0ce18336b
TArch64 <tarasturchenko1551@gmail.com>
2023-02-24 19:13:15 +0200

    use date contructor

--- frontend/composables/use-live-time.ts
+++ frontend/composables/use-live-time.ts
@@ -1,12 +1,12 @@
 import type { Ref } from 'vue';
 
 export function useLiveTime(): Ref<Date> {
-    const now = ref(Date.now());
+    const now = ref(new Date());
 
     if (process.client) {
-        const intervalId = setInterval(() => now.value = Date.now(), 5000);
+        const intervalId = setInterval(() => now.value = new Date(), 5000);
         onUnmounted(() => clearInterval(intervalId));
     }
 
-    return computed(() => new Date(now.value));
+    return computed(() => now.value);
 }
